d4bedd5055bcf014d3138932c472f7802967fdff
Valentyn <xdshkaa2@gmail.com>
2023-07-01 22:14:18 +0300

    master: changed event

--- src/app.mjs
+++ src/app.mjs
@@ -2,8 +2,8 @@ import crypto from 'crypto';
 import browserEnv from './browserEnv.mjs';
 import { getProxyAgent } from '../libs/proxyAgent.mjs';
 
-const eventId = '00005E66AD4B2014';
-const cookies = 'reese84=3:OqR1kHkkfaT1DkL8ElJ3tA==:8RpiR4gm/tyK+U9ugHTmJZMIg5lH8lCyJQKRhK6XhkgRGnAWWw40Wqi9VDdIrPMiQaoRkMPJfZowFMjE8TMbtnzzMxuHPRfZjsrtE9PiLcRVue9an6P/xdxk8kUMNCLYHNtj3bb/fEeFwvvP8YvGKIU70+rLanqRbC3hTjJ2Kt2OYFDxqAhRn+Veey2114QOAlzFsb+rwjTKY3xgpbfOG0SltyHfX+s5rdClWbXFI1NWkW/CfqDOz/Xvxu38CfmwnFJ5vAVJ/3c2QfF51d71Z6B3HqjSRXiMQpK22xToUkkP1w05uEy6rwEfFDZ59Z/+ETis0yJZpbULbq8cPDlBg8sLSoL+nA9cgSCmUT+eHovNpQH89BuuN7tq1phMED+/vXLkho47M7dNKBB0nxCRnUkTNGKtn9P57LAdbNNwt8PB891fQMhBWyjYssYDw/8GwxhnL7CY6HCt+VKR3dLYZQ==:wj3/RD42ATfwD5zUe8t7U4Zon78aBG4ppFXYchyU5ks=';
+const eventId = '0F005E5CD4FC2F47';
+const cookies = 'reese84=3:Kg+gv3FnQ5BDa8kIBBEepg==:uy3PPZ4T7C3bzRr77zLDOSNzikC7roh/1isF+2cSZP+mcUTllUxTr6fR+cZGPMrqxjjviOAevzhtWWpxdYYUcJ+ntvi9qSi7zCiuKgLngkUHgk9n5SO8DhG6aWkXb9HO6Ke33WKja4WRC24pSpVdE8VNaseW8jKg52+xr0fFy9pbLhVBo206Ss0hpLfISPb9dOft7GQ6PnbqcjWLSm4/iiX5MTMwaI23+AQwRryMMjDpoEUj+ozaOm4/S5xhrOYkZWI6iiHgJZ0JsKiebZrqy7V68hUz7vF5rz5RMh5zOmkF8QqqCH0w1M2/xtKsTpgcaiXgZ1XlyBOnjVF/McW/OmJ4stYIjt1kV4s5xnfbm9VgXUMKoNWFI2o84sX//MSzhKZYaLvM1vHIrx6MsDRvdivyQxkbyemjLZuJ7rjXDY7V5PB0lU3+kF9pUy3uCmijrsqyCVBZJqEVdY+zEdywbK4AdnEh/iEvaxxPEuabrGmVv/3aYGqRuoEZNqFyJvLCcgrhH7mR/uUmJShLcvhD70jREGrVNZ9YOsYLaW5f3BkxoekqaM7/JMQMUDSKtvcz:/m8HwkNM3D+2R7eWh27gGAh02KO3WqFa80NSbH1eOL4=';
 
 browserEnv({
   url: `https://concerts.livenation.com/event/${eventId}`,
@@ -14,6 +14,7 @@ self.fetch = fetch;
 self.crypto = crypto;
 window.crypto = crypto;
 window.proxyAgent = getProxyAgent();
+window.encodeURIComponent = encodeURIComponent;
 
 const setCookies = (cookies) => {
   const arr = cookies.split('; ');
@@ -27,6 +28,6 @@ const setCookies = (cookies) => {
   const instance = await sdk.default.init({ eventId });
   console.log('instance', instance);
 
-  const checkout = await instance.checkout({ eventId, "requestorId": "f38ce660-16a0-11ee-a669-d9853f663d51", "requestContext": { "localeInjected": "en-us", "locale": "en-us" }, "tickets": [{ "inventoryDetail": { "type": "Primary" }, "row": "A", "section": "SECA", "ticketTypes": [{ "id": "000000000001", "quantity": 4 }] }] }, { "appview": false, "appviewLN": false, "appviewVersion": 1, "toolspreview": false, "channel": 0, "layout": "", "redirect": true, "source": 2 });
+  const checkout = await instance.checkout({ eventId, "requestorId": "f38ce660-16a0-11ee-a669-d9853f663d51", "requestContext": { "localeInjected": "en-us", "locale": "en-us" }, "tickets": [{ "inventoryDetail": { "type": "Primary" }, "row": "4", "section": "RSLWNB", "ticketTypes": [{ "id": "000000000001", "quantity": 1 }] }] }, { "appview": false, "appviewLN": false, "appviewVersion": 1, "toolspreview": false, "channel": 0, "layout": "", "redirect": true, "source": 2 });
   console.log('checkout', checkout);
 })();
