24a52c3b5b0e121ddf62e3e401ed0e98f5533edf
Valentyn <xdshkaa2@gmail.com>
2023-07-14 23:12:04 +0300

    check

--- rasSdk/app.ts
+++ rasSdk/app.ts
@@ -8,7 +8,9 @@ import { fetchLocal } from '../libs/nodeFetch';
 import { getProxyAgent } from '../libs/proxyAgent';
 import { initJSDOM, getUserAgent } from '../libs/jsdom';
 
-const eventId = '15005E58EEB15F8A';
+const eventId = '0F005E5CD4FC2F47';
+const eventId2 = '05005D7EF30764C5';
+
 const sessionId = '3:UNfH9dHE4Bhz1UVu5wGBKw==:dcZEmELabu6TDlcqPbFKKCc31Ue6Xj2fGMVW08KY78sK8GMf0wB+1EeerHmM/cj6m/K+VO6MoefYNCKsgcqZrRPAZfGjXwbufbM/eFuf4qyqXszqq3QpHOSpsZED5HNYB+TyVKIgv121/Tk68t30PD0nbxqiKDcUlgHrzsOzFwGhj1PGE791P7oT08RaCUnIrUtitA+Ej5UTqSU4NqWS9ISwectKb60b6wCAHF1TNWU2AUmV3W93jFmE/0rMutuQhx8/fnHsrHZxFV497QDxVSi8jtVaF80N4p2SyuBPepcUbVKP2KjLr4vtinCl2sOeugXmongw1Fu6PrenVfqIOd03NPYEabfrRiw+nngEcKgh6YjCXhmXdSIZ7M17E4Q7l8JdYzNqBMMftXy9DVZ78iaDPjjKgYx9ptGs109ABphQSg01oZ8Ut5+yR9tU2GvxZNQlrT5wbzuQa1tPJa6d5Q==:0DM9b1tf2nes/xMRqSh6OBzNKpwrwgmqKOMeRy4Rxog=';
 
 const findOutWhatReleasedAndWhatRemoved = (oldSet: Set<unknown>, newSet: Set<unknown>) => {
@@ -49,6 +51,7 @@ const subsOnce = async (eventId: string) => {
   asd.subscribe({
     next: (t: any) => {
       const e = t.getAvailablePlaceIds();
+      console.log('asd.subsctibe', e);
       const newSet = new Set(e);
       const { removed, released } = findOutWhatReleasedAndWhatRemoved(oldSet, newSet);
 
@@ -59,6 +62,15 @@ const subsOnce = async (eventId: string) => {
       }
 
       oldSet = newSet;
+
+      const asd2 = created.observeEventAvailability(eventId2, {});
+      asd2.subscribe({
+        next: (t: any) => {
+          const e = t.getAvailablePlaceIds();
+          console.log('asd2.subsctibe', e);
+        },
+      });
+        
     },
   });
 };
